c94636e47ddf1add64f7c6b0055146b41175efdc
Mauro-at-686601635418 <maurojuarezarino@gmail.com>
2020-03-08 12:32:26 -0300

    add: Basic UI for References tools

---
+++ UnityToolSuite/AssetsTools/ReferencesTool/ReplaceReferencesWindow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ToolSuite.AssetTools.ReferencesTools
+{
+    public class ReplaceReferencesWindow : EditorWindow
+    {
+        public const string MENU_NAME = "ReplaceReferences"; 
+
+        private Object originalAsset = null;
+        private Object replaceAsset = null;
+        private Vector2 scrollPosition = Vector2.zero;
+
+        [MenuItem(ToolSuiteMenuName.MENU_NAME + "/" + AssetToolsMenuName.MENU_NAME + "/" + MENU_NAME)]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<ReplaceReferencesWindow>();
+            window.titleContent = new GUIContent(MENU_NAME);
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            if (EditorSettings.serializationMode != SerializationMode.ForceText)
+            {
+                return;
+            }
+
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+
+            originalAsset = EditorGUILayout.ObjectField(new GUIContent("Original Asset: "), originalAsset, typeof(Object), false);
+
+            replaceAsset = EditorGUILayout.ObjectField(new GUIContent("Replace references with: "), replaceAsset, typeof(Object), false);
+
+            GUILayout.Button("Replace references");
+
+            EditorGUILayout.EndScrollView();
+        }
+    }
+}
---
+++ UnityToolSuite/AssetsTools/ReferencesTool/SearchReferencesWindow.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ToolSuite.AssetTools.ReferencesTools
+{
+    public class SearchReferencesWindow : EditorWindow
+    {
+        public const string MENU_NAME = "SearchReferences"; 
+
+        private Object assetToSearchReferencesFor = null;
+        private Vector2 scrollPosition = Vector2.zero;
+
+        [MenuItem(ToolSuiteMenuName.MENU_NAME + "/" + AssetToolsMenuName.MENU_NAME + "/" + MENU_NAME)]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<SearchReferencesWindow>();
+            window.titleContent = new GUIContent(MENU_NAME);
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            if (EditorSettings.serializationMode != SerializationMode.ForceText)
+            {
+                return;
+            }
+
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+
+            assetToSearchReferencesFor = EditorGUILayout.ObjectField(new GUIContent("Asset: "), assetToSearchReferencesFor, typeof(Object), false);
+
+            GUILayout.Button("Search references");
+
+            EditorGUILayout.EndScrollView();
+        }
+    }
+}
